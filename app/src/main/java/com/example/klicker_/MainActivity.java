6192180a67e8619231c6bf14705d32d34e7b242d
ytka2235 <ytka2235@gmail.com>
2023-03-17 19:00:43 +0300

    Initial commit

---
+++ app/src/main/java/com/example/klicker_/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.klicker_;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    private Integer counter = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickBtnAddStudents(View view) {
+        counter++;
+        TextView counterView = (TextView)findViewById(R.id.txt_counter);
+        counterView.setText(counter.toString());
+    }
+
+}
